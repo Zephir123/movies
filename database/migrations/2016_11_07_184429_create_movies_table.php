b074341462aa9a1771f7e329042706043c56c02c
Colin Bennett <cbennett@sans.org>
2016-11-07 16:11:17 -0500

    Added migrations, model, and controller.

---
+++ database/migrations/2016_11_07_184429_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',50);
+            $table->integer('format');
+            $table->integer('length');
+            $table->integer('release_year');
+            $table->integer('rating')->nullable();
+
+            //$table->foreign('format')->references('format')->on('formats');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movies');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         // $this->call(UsersTableSeeder::class);
+        $this->call(SeedFormatsTable::class);
     }
+}
+
+class SeedFormatsTable extends Seeder
+{
+
+    public function run()
+    {
+        //delete users table records
+        DB::table('formats')->delete();
+        //insert some dummy records
+        DB::table('formats')->insert(array(
+            array('format'=>'VHS'),
+            array('format'=>'DVD'),
+            array('format'=>'Streaming'),
+        ));
+    }
+
+    public function down()
+    {
+        DB::table('formats')->delete();
+    }
+
 }
